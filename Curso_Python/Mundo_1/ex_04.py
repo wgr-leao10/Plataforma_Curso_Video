d089577d7b5e675494658a7a2d3885a62b3f9088
wgr-leao10 <wgr.leao10@gmail.com>
2024-01-29 21:30:51 -0300

    Create ex_04.py

---
+++ Curso_Python/Mundo_1/ex_04.py
@@ -0,0 +1,12 @@
+algo = input("Digite Algo: ")
+print('O tipo primitivo deste valor é? ', type(algo))
+print("Só tem espaços?", algo.isspace())
+print("É número?", algo.isspace())
+print("É Alfabético?", algo.isalpha())
+print("É Alfanúmerico?", algo.isalnum)
+print("Está em Maíscula?", algo.isupper())
+print("Esta´em Minúscula?", algo.islower())
+print("Está Capitalizada?", algo.istitle())
+
+
+
