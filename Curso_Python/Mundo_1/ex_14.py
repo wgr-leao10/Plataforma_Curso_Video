46d77c92de7f40309df54b136f14108daa93cf89
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-02-13 11:16:10 -0300

    Create ex_14.py

---
+++ Curso_Python/Mundo_1/ex_14.py
@@ -0,0 +1,6 @@
+print("-=" * 35)
+print("Conversor de Temperaturas")
+print("-=" * 35)
+temperatura_c = int(input("Digite a Temperatura em  C: "))
+mperatura_f = (temperatura_c * 9/5) + temperatura_c
+print(f"A temperatura em {temperatura_c}C é equivalente a {mperatura_f}F")
