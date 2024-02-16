c02469b0f6b7975b68017990d79c855d22c2fb26
wgr-leao10 <wgr.leao10@gmail.com>
2024-02-16 10:26:34 -0300

    Create ex_15.py

---
+++ Curso_Python/Mundo_1/ex_15.py
@@ -0,0 +1,9 @@
+print("-=" * 35)
+print("Cáculo do Aluguel")
+print("-=" * 35)
+km = int(input("Quantos Km Rodados: "))
+dias_usado = int(input("Quantos dias Alugados: "))
+valor_diario = 60
+valor_km = 0.15
+caculo_aluguel = (km * valor_km) + (dias_usado * valor_diario)
+print(f"O valor total do Aluguel é R$: {caculo_aluguel:.2f}")
