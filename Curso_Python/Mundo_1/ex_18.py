7aecd249759484f88830aef0aa2c5ed9e0036ce5
wgr-leao10 <wgr.leao10@gmail.com>
2024-02-26 12:13:39 -0300

    Create ex_18.py

---
+++ Curso_Python/Mundo_1/ex_18.py
@@ -0,0 +1,8 @@
+from math import sin, cos, tan
+print("-=" * 15)
+print("Calculo de Angulo")
+print("-=" * 15)
+num = int(input("Digite o Angulo que voce deseja: "))
+seno = sin(num)
+coseno = cos(num)
+tangente  = tan(num)
