223e3f91f2728b68f88501a8d84aaaf6848b4a8a
wgr-leao10 <wgr.leao10@gmail.com>
2024-03-22 11:20:51 -0300

    Create ex_25.py

---
+++ Curso_Python/Mundo_1/ex_25.py
@@ -0,0 +1,6 @@
+print("-=" * 15)
+print("Analisando Nome")
+print("-=" * 15)
+nome = str(input("Qual seu nome Completo: "))
+verificacao = "Silva" in nome  # Diferenciar maiuscula de minusculo
+print(f"Seu nome tem Silva? {verificacao}")
