22342c5ff560faa1306fd628fa02f3e92547b550
wgr-leao10 <wgr.leao10@gmail.com>
2024-03-28 18:22:25 -0300

    Update ex_24.py

--- Curso_Python/Mundo_1/ex_24.py
+++ Curso_Python/Mundo_1/ex_24.py
@@ -1,5 +1,5 @@
 print("-=" * 15)
 print("Analisando Cidade de Origem")
 print("-=" * 15)
-cidade = str(input("Digite a Cidade: "))
-print("Guarulhos" in cidade)
+cidade = str(input("Digite a Cidade: ")).strip()
+print(cidade[:5].upper() == "SANTO")
