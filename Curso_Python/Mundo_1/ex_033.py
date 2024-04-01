4daafc8da017ee0068d19a3cb9cf2cfc5b3f1ca0
wgr-leao10 <wgr.leao10@gmail.com>
2024-04-01 18:15:14 -0300

    Create ex_033.py

---
+++ Curso_Python/Mundo_1/ex_033.py
@@ -0,0 +1,8 @@
+print("-=" * 25)
+print("Qual o Maior Número?")
+print("-=" * 25)
+num_1 = int(input("Primeiro Valor: "))
+num_2 = int(input("Segundo Valor: "))
+num_3 = int(input("Terceiro Valor: "))
+if num_1 > num_2 or num_1 > num_3:
+    print(f"O maior Valor digititado {num_1}")
