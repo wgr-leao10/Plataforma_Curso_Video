c96ca1d43ba199378602cd4b0bd52999a3253d53
wgr-leao10 <wgr.leao10@gmail.com>
2024-04-01 18:14:49 -0300

    Create ex_30.py

---
+++ Curso_Python/Mundo_1/ex_30.py
@@ -0,0 +1,8 @@
+print("-=" * 25)
+print("PAR OU IMPAR")
+print("-=" * 25)
+num = int(input("Me Diga Um Número Inteiro Qualquer: "))
+if num % 2 == 0:
+    print(f"O número {num} é par")
+else:
+    print(f"O número {num} é impar")
