a7be22cc5d6a63f22a81786935ecaf02138edf23
wgr-leao10 <wgr.leao10@gmail.com>
2024-04-03 21:06:23 -0300

    Update ex_033.py

--- Curso_Python/Mundo_1/ex_033.py
+++ Curso_Python/Mundo_1/ex_033.py
@@ -4,5 +4,16 @@ print("-=" * 25)
 num_1 = int(input("Primeiro Valor: "))
 num_2 = int(input("Segundo Valor: "))
 num_3 = int(input("Terceiro Valor: "))
-if num_1 > num_2 or num_1 > num_3:
-    print(f"O maior Valor digititado {num_1}")
+if num_1 > num_2 and num_1 > num_3:
+    print(f"O Maior Valor Digititado {num_1}")
+if num_2 > num_1 and num_2 > num_3:
+    print(f"O Maior Valor Digititado {num_2}")
+if num_3 > num_1 and num_3 > num_2:
+    print(f"O Maior Valor Digitado {num_3}")
+if num_1 < num_2 and num_1 < num_3:
+    print(f"O Menor Valor Digititado {num_1}")
+if num_2 < num_1 and num_2 < num_3:
+    print(f"O Menor Valor Digititado {num_2}")
+if num_3 < num_1 and num_3 < num_2:
+    print(f"O Menor Valor Digitado {num_3}")
+print("Bom Dia")
