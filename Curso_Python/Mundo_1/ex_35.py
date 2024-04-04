48b76f5078481eabe4f8ff9623f37df02c12450d
wgr-leao10 <wgr.leao10@gmail.com>
2024-04-03 21:06:30 -0300

    Create ex_35.py

---
+++ Curso_Python/Mundo_1/ex_35.py
@@ -0,0 +1,10 @@
+print("-=" * 25)
+print("Forma Um Triagulo")
+print("-=" * 25)
+seguimento_1 = float(input("Primeiro Seguimento: "))
+seguimento_2 = float(input("Segundo Seguimento: "))
+seguimento_3 = float(input("Terceiro Seguimento: "))
+if seguimento_1 + seguimento_2 > seguimento_3:
+    print("Estes Seguimetos FORMAM UM TRIÂNGULO")
+else:
+    print("Estes Seguimentos NÃO FORMAM UM TRIÂNGULO")
