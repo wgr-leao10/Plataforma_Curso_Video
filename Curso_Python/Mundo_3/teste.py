aa8a54504d239d1c8226b0192b043a50aea5d21c
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-05-01 16:34:25 -0300

    Update teste.py

--- Curso_Python/Mundo_3/teste.py
+++ Curso_Python/Mundo_3/teste.py
@@ -1,4 +1,15 @@
-import math
-num = int(input("Digite um número para saber sua raiz quadrada: "))
-raiz_quadrada = math.sqrt(num)
-print(f"A raiz quadrada de {num} é {raiz_quadrada}")
+lanche = ("Hamburger", "Suco", "Pizza", "Pudim", "Batata Frita")
+
+for cont in range(0, len(lanche)):
+    print("Eu vou comer para caramba", lanche[cont])  # Lanche na posição cont
+
+# Técnica 3, nesta da para mostrar a posição, como exemplo abaixo:
+
+for cont in range(0, len(lanche)):
+    print(f"Eu vou comer para caramba {lanche[cont]}, na posição {cont}")
+
+# Técnica 4, nesta da para mostrar a posição, usando outra técnica:
+
+for pos, comida in enumerate(lanche):  # o enumerate ele numera os itens
+    print(f"Eu comi para caramba {comida}, na posição {pos}")
+
