829af3e907b6692d3acb09f400f3fb6be5c35258
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-05-01 16:34:12 -0300

    Create Mundo_3_aula_17_lista_pt1.py

---
+++ Curso_Python/Mundo_3/Mundo_3_aula_17_lista_pt1.py
@@ -0,0 +1,15 @@
+num = [2, 5, 9, 1]
+num[2] = 3
+num.append(7)  # acrescentar item
+num.sort()  # ordenar item
+num.sort(reverse=True)  # reverter a ordem
+num.insert(2, 0)  # aqui é para inserir, se lê inserir item 0 na posição 2
+num.pop()  # aqui deleta o ultimo elemento da lista
+num.pop(2)  # aqui deleta o iem na posição 2
+num.remove(2)  # semelhante ao anterior remove o item na índece 2
+if 8 in num:  # Aqui verifica se existe o índice
+    num.remove(8)  # se for vrdadeiro ele remove
+else:
+    print("nÃO ACHEI O 8")
+print(num)
+print(f'Esta lista tem {len(num)} itens')
