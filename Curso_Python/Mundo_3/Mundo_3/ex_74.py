6c49bfb9ceb38ad7a2bd569f88f8064fb2226bc9
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-05-01 16:33:56 -0300

    Delete ex_74.py

--- Curso_Python/Mundo_3/Mundo_3/ex_74.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-print("-=" * 25)
-print("Sorteio de Números")
-print("-=" * 25)
-sorteio = (randint(0, 10), randint(0, 10), randint(0, 10),
-           randint(0, 10), randint(0, 10))
-print(f'Os Numeros sorteados são:{sorteio}')
-print(f'O maior valor Sorteado foi {max(sorteio)}')
-print(f'O menor valor sorteado foi {min(sorteio)}')
