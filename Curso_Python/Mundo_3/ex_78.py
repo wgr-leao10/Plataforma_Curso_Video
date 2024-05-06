4849128b055d864ea6b7e33987adca2e159aa4c1
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-05-06 20:56:26 -0300

    Update ex_78.py

--- Curso_Python/Mundo_3/ex_78.py
+++ Curso_Python/Mundo_3/ex_78.py
@@ -16,6 +16,7 @@ for i in range(0, 5):
 print(f'Você digitou os valores{numeros}')
 print(f'O maior Valor digitado {max(numeros)}, nas posiçoes {i}...{i}')
 print(f'O menor Valor digitado {min(numeros)}, nas posições {i}')
+print()
 print(f'O maior Valor digitado {maior}, nas posições', end=' ')
 for j, c in enumerate(numeros):
     if c == maior:
