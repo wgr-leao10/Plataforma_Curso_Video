d9958258860f110b1c59a8a06245a612eab23b74
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-05-22 20:52:27 -0300

    Create Mundo_3_aula_17_listapt2.py

---
+++ Curso_Python/Mundo_3/Mundo_3_aula_17_listapt2.py
@@ -0,0 +1,9 @@
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])  # no amterial da matéria a um relatório detalhado
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])  # Este comando é importante
+print(galera)
