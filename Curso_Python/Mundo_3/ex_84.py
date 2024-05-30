6414bd1a31b5a0f258e253867c6b554ea1420430
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-05-30 20:29:31 -0300

    Update ex_84.py

--- Curso_Python/Mundo_3/ex_84.py
+++ Curso_Python/Mundo_3/ex_84.py
@@ -1,5 +1,6 @@
 print("-" * 40)
 print(f'{"Lista Composto e Análise de Dados EX_83":^40}')
+print(f'{"Lista Composto e Análise de Dados EX_84":^40}')
 print("-" * 40)
 cadastro = list()
 dados = list()
@@ -7,13 +8,19 @@ i = totalcadastro = maiorpeso = menorpeso = 0
 while i != 'N':
     dados.append(str(input('Nome: ')))
     dados.append(int(input('Peso: ')))
+    dados.append(int(input('Peso Kg: ')))
     if len(cadastro) == 0:
         maiorpeso = menorpeso = cadastro[1]
+        maiorpeso = menorpeso = dados[1]
     else:
         if cadastro[1] > maiorpeso:
             maiorpeso = cadastro[1]
         if cadastro[1] < menorpeso:
             menorpeso = cadastro[1]
+        if dados[1] > maiorpeso:
+            maiorpeso = dados[1]
+        if dados[1] < menorpeso:
+            menorpeso = dados[1]
     cadastro.append(dados[:])
     dados.clear()
     i = ' '
