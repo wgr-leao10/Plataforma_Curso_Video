d0192f3ec271832103a69fc27497edab1467f5bd
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-06-20 09:54:11 -0300

    Update ex_85.py

--- Curso_Python/Mundo_3/ex_85.py
+++ Curso_Python/Mundo_3/ex_85.py
@@ -3,6 +3,7 @@ print(f'{"Lista com Pares e Impares EX_85":^40}')
 print("-" * 40)
 numeros = [[], []]
 for i in range(0, 7):
+for i in range(1, 8):
     n = int(input(f"Digite o {i}° valor: "))
     if n % 2 == 0:
         numeros[0].append(n)
