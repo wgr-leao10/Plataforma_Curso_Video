c1078cc21704b944fd5279ebedd15d3ada1df4c5
wgr-leao10 <wagnerribeiro.dev@gmail.com>
2024-07-02 10:09:51 -0300

    Update Mundo_3_aula_17_listapt2.py

--- Curso_Python/Mundo_3/Mundo_3_aula_17_listapt2.py
+++ Curso_Python/Mundo_3/Mundo_3_aula_17_listapt2.py
@@ -6,4 +6,5 @@ galera.append(teste[:])  # no amterial da matéria a um relatório detalhado
 teste[0] = 'Maria'
 teste[1] = 22
 galera.append(teste[:])  # Este comando é importante
+galera.append(teste[:])  # Este comando é importante pois aqui ele faz a cópia
 print(galera)
